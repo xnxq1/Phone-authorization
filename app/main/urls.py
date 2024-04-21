803e71ebce58895411a6b6f979cec454f34d6a12
xnxq1 <vasya3222@inbox.ru>
2024-04-22 02:18:44 +0300

    Add LoginView

---
+++ app/main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('login/', views.Login.as_view(), name='login'),
+]
